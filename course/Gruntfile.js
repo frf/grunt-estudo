34465ed3194f7e5b6bf1f91e74cc47b9203d98ed
Fabio <fabio@fabiofarias.com.br>
2016-09-06 01:28:35 -0300

    Estudo Grunt

--- course/Gruntfile.js
+++ course/Gruntfile.js
@@ -3,6 +3,8 @@
  */
 module.exports = function( grunt ){
 // Project configuration.
+    var mozjpeg = require('imagemin-mozjpeg');
+
     grunt.initConfig({
         uglify: {
             options: {
@@ -14,17 +16,87 @@ module.exports = function( grunt ){
                 }
             }
         },//uglify
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'assets/css/style.min.css': 'assets/_sass/style.scss'
+                }
+            }
+        },
         watch: {
             dist: {
-                files: ['assets/_js/**/*'],
-                tasks: ['uglify']
+                files: ['assets/_js/**/*','assets/html/**/*'],
+                tasks: ['uglify','htmlmin'],
+                options:{
+                  //  livereload:true
+                }
             },
         },
+        shell:{
+            options:{
+                stderr:false
+            },
+            mkdir:{
+                command: function(dir){
+                    return 'mkdir '+ dir;
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                options: {
+                    optimizationLevel: 3,
+                    svgoPlugins: [{ removeViewBox: false }],
+                    use: [mozjpeg()]
+                },
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'assets/_img',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'assets/img'                  // Destination path prefix
+                }]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'index.html': 'assets/html/index.html',
+                }
+            },
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    hostname: '*',
+                  //  livereload:true
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('default',['uglify']);
+    grunt.registerTask('default',['uglify','sass','htmlmin']);
+    grunt.registerTask('sass',['sass']);
     grunt.registerTask('w',['watch']);
+    grunt.registerTask('mk', function(dir){
+        grunt.task.run('shell:mkdir:' + dir);
+    });
+    grunt.registerTask('img',['imagemin']);
+    grunt.registerTask('html',['htmlmin']);
+    grunt.registerTask('server',['connect','watch']);
 };
--- course/assets/_js/main.js
+++ course/assets/_js/main.js
@@ -2,7 +2,7 @@
  * Created by fabio rocha on 05/09/16.
  */
 (function(document){
-    document.getElementById("main").innerHTML = '<section><h1>This is my S in Action, Watch</h1></section>';
-    document.getElementById("main2").innerHTML = '<section><h1>Hello World, Watch</h1></section>';
-    document.getElementById("main3").innerHTML = '<section><h1>Hello World 3, Watch</h1></section>';
+    document.getElementById("header").innerHTML = '<section><h1>This is my JS in Action, Watch</h1></section>';
+    document.getElementById("main").innerHTML = '<section><h1>Main</h1></section>';
+    document.getElementById("footer").innerHTML = '<section><h1>Footer</h1></section>';
 })(document);

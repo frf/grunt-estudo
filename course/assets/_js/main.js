63d791f64823adeff0745ce3fcac2bdcb773eb22
Fabio <fabio@fabiofarias.com.br>
2016-09-05 23:43:15 -0300

    Estudo Grunt

--- course/assets/_js/main.js
+++ course/assets/_js/main.js
@@ -1,6 +1,7 @@
 /**
- * Created by fabior on 05/09/16.
+ * Created by fabio rocha on 05/09/16.
  */
 (function(document){
     document.getElementById("main").innerHTML = '<section><h1>This is my S in Action, Watch</h1></section>';
+    document.getElementById("main2").innerHTML = '<section><h1>Hello World, Watch</h1></section>';
 })(document);
